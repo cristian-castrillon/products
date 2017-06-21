1c451405dd12b0337e974444f237df15cc0f4ea7
cristian-castrillon <cristian.castrillon@gmail.com>
2017-06-21 17:09:05 -0500

    [ADD] Reto para manejo de asociaciones muchos a muchos

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,53 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def new
+    @product = Product.new
+    @categories = Category.all
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      @product.categories << Category.find(params[:product][:category_ids])
+
+      redirect_to(products_path)
+    else
+      @errors = @room.error.full_messages
+      render 'products/new'
+    end
+  end
+
+  def edit
+    begin
+      @product = Product.find(params[:id])
+      @categories = Category.all
+    rescue ActiveRecordNotFound
+      redirect_to(products_path)
+    end
+  end
+
+  def update
+    params[:product][:category_ids] ||= []
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      @product.categories.destroy_all
+      @product.categories << Category.find(params[:product][:category_ids])
+      redirect_to(products_path)
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @product = Product.destroy(params[:id])
+    redirect_to(products_path)
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price)
+  end
+end
